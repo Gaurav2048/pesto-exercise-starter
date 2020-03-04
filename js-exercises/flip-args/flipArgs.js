0ff175447d5e44f97b0ab7df38e05483f009cbc0
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-03-05 00:28:24 +0530

    FlipArgs program continues

--- js-exercises/flip-args/flipArgs.js
+++ js-exercises/flip-args/flipArgs.js
@@ -1,3 +1,12 @@
-function flipArgs() {
-  
+function flipArgs(func) {
+  // eslint-disable-next-line func-names
+  return function () {
+    // eslint-disable-next-line prefer-rest-params
+    func(...Array.from(arguments).reverse());
+  };
+}
+
+module.exports = {
+  flipArgs,
+// eslint-disable-next-line semi
 }
