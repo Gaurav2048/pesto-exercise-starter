abd01f65c5af89b0b7a1ee0f16882fa33a0d1e8e
Gaurav2048 <gunjankalita836@gmail.com>
2020-03-04 10:26:09 +0530

    Solved duplicate letter assignent

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,8 +1,33 @@
 
 function duplicateLetters(...args) {
-  return args;
+  var string = args[0]
+  var max_occurance_of_character = 1
+  while (string.length !== 0) {
+    var current_character = string.charAt(0);
+    var occurance_of_current_character = 1
+    string = remove_character(string, 0)
+    for (var j = 0; j < string.length; j++) {
+      if (string.charAt(j) === current_character) {
+        remove_character(string, j)
+        occurance_of_current_character++
+      }
+    }
+    if (occurance_of_current_character > max_occurance_of_character) {
+      max_occurance_of_character = occurance_of_current_character
+    }
+  }
+  if (max_occurance_of_character === 1) {
+    return false
+  }
+  return max_occurance_of_character;
 }
 
-export {
+function remove_character(str, char_pos) {
+  var part1 = str.substring(0, char_pos);
+  var part2 = str.substring(char_pos + 1, str.length);
+  return (part1 + part2);
+}
+
+module.exports = {
   duplicateLetters,
 };
