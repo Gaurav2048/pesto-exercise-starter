f7662b9b19afcc1a5f95ea0f3df3d32a69175d67
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-03-04 19:40:04 +0530

    Solved curry problem

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,19 +1,16 @@
 
-function curry(fn){
- 
- return function fn_2(x){
-   return function fn_3(y){
-      return fn(x,y)
+function curry(fn) {
+  return function ineerFn(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
     }
-  }
+    // eslint-disable-next-line func-names
+    return function (...args2) {
+      return ineerFn.apply(this, args.concat(args2));
+    };
+  };
 }
-const add = curry((a, b) => {
-  return a + b;
-});
 
-console.log(add(1)(2));
-
-
-// export {
-//   curry,
-// };
+module.exports = {
+  curry,
+};
